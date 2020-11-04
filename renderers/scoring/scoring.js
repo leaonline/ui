5ab23ca70516904598bc939cd65acc11b18b752c
Jan Küster <jkuester@uni-bremen.de>
2020-11-04 15:19:52 +0100

    renderers scoring resolve representative using resolveRepresentative method

--- renderers/scoring/scoring.js
+++ renderers/scoring/scoring.js
@@ -1,12 +1,13 @@
 import { Template } from 'meteor/templating'
 import { Competency } from 'meteor/leaonline:corelib/contexts/Competency'
 import { getCollection } from 'meteor/leaonline:corelib/utils/collection'
+import { resolveRepresentative } from 'meteor/leaonline:corelib/utils/resolveRepresentative'
 import '../../components/icon/icon'
 import './scoring.html'
 
 Template.itemScoringRenderer.helpers({
   getCompetency (_id) {
     const competencyDoc = getCollection(Competency.name).findOne(_id) || {}
-    return competencyDoc[Competency.representative] || _id
+    return resolveRepresentative(competencyDoc, Competency.representative)
   }
 })
