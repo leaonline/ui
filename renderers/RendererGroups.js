c0506cbbde09f7703c0c54ad93cf24b4df6f7629
Jan Küster <jkuester@uni-bremen.de>
2020-11-05 12:27:44 +0100

    renderers RendererGroups added flag to indicate task page renderers

--- renderers/RendererGroups.js
+++ renderers/RendererGroups.js
@@ -1,14 +1,17 @@
 export const RendererGroups = {
   layout: {
     name: 'layout',
+    isTaskContent: true,
     label: 'taskRenderers.layout.title'
   },
   items: {
     name: 'items',
+    isTaskContent: true,
     label: 'taskRenderers.items.title'
   },
   documents: {
     name: 'document',
+    isTaskContent: false,
     label: 'taskRenderers.documents.title'
   }
 }
